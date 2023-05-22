5f330954c3dda18835f2425173e2df5667d20852
Shadat Rahman <shadat.rahman.464@gmail.com>
2023-05-22 15:50:55 +0600

    init

---
+++ mysite/urls.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.urls import path
+from core.views import index
+urlpatterns = [
+    path('', index, name='index'),
+    path('admin/', admin.site.urls),
+]
